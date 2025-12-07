1412de579af6b9f4a3624b5e482d79025ba63db5
ogazboiz <ogazboizakpolo@gmail.com>
2025-12-07 19:12:46 +0100

    feat: extend contract with Ownable and ReentrancyGuard

--- contracts/VaultFactory.sol
+++ contracts/VaultFactory.sol
@@ -4,3 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title VaultFactory
+ * @dev Factory contract for creating and managing ERC-4626 vaults
+ * @notice Users must register before creating vaults
+ */
+contract VaultFactory is Ownable, ReentrancyGuard {
+
